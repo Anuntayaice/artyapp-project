cf669599dc9a260e2d000e872cc0d885905761c8
Anuntayaice <iceanuntaya.5@gmail.com>
2023-11-26 23:09:45 +0100

    card format edited

--- src/Pages/Exercises.js
+++ src/Pages/Exercises.js
@@ -3,7 +3,18 @@ import ExCard from "../components/cards/ExCard";
 const Exercises = () => {
   return (
     <div className="bg-secondary d-flex align-items-center justify-content-center vh-100">
-      <ExCard imageSrc="loginpic.png" />
+      <ExCard
+        imageSrc="expic.png"
+        text="Once upon a time, in a magical garden filled with the most
+              enchanting flowers, there lived a kind squirrel named Sam. Sam
+              loved spending his days on a cozy wooden bench nestled amidst the
+              blossoms. One sunny morning, as Sam sat on his favorite spot, he
+              noticed a charismatic butterfly gracefully gliding through the
+              air. Mesmerized by its colorful wings, Sam couldn't help but feel
+              a sense of wonder. Little did he know that this encounter would
+              turn into an extraordinary friendship, filled with exciting
+              adventures and heartwarming moments."
+      />
     </div>
   );
 };
--- src/components/cards/ExCard.js
+++ src/components/cards/ExCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Card, Button, Row, Col, Image } from "react-bootstrap";
 import ProgressBar from "react-bootstrap/ProgressBar";
-const ExCard = ({ imageSrc }) => {
+const ExCard = ({ imageSrc , text}) => {
   return (
     <Card
       className="text-center mt-4"
       style={{
         maxWidth: "80rem",
         minWidth: "75em",
-        maxHeight: "48rem",
+        maxHeight: "45rem",
         borderRadius: "16px",
         backgroundColor: "rgba(232, 230, 230, 0.2)",
       }}
@@ -17,7 +17,7 @@ const ExCard = ({ imageSrc }) => {
         className="border-0 d-flex justify-content-center align-items-center"
         style={{ height: "5em" }}
       >
-        <Row className="w-100 align-content-center align-items-center">
+        <Row className="w-100 align-content-center align-items-center flex-fill">
           <Col xs={1} className="text-start px-3">
             <Button
               variant="link"
@@ -44,22 +44,14 @@ const ExCard = ({ imageSrc }) => {
         <Row>
           <Col>
             {" "}
-            <Image src={imageSrc} fluid />
+            <Image src={imageSrc} fluid style={{ maxHeight: "100%" }} />
           </Col>
           <Col className="d-flex flex-column">
             <Card
               className="px-5 text-start pt-3 pb-5 border-0 mb-3"
               style={{ lineHeight: "2.5", borderRadius: "20px" }}
             >
-              Once upon a time, in a magical garden filled with the most
-              enchanting flowers, there lived a kind squirrel named Sam. Sam
-              loved spending his days on a cozy wooden bench nestled amidst the
-              blossoms. One sunny morning, as Sam sat on his favorite spot, he
-              noticed a charismatic butterfly gracefully gliding through the
-              air. Mesmerized by its colorful wings, Sam couldn't help but feel
-              a sense of wonder. Little did he know that this encounter would
-              turn into an extraordinary friendship, filled with exciting
-              adventures and heartwarming moments.
+              {text}
             </Card>
             <Button className="align-self-end mt-auto" style={{ width: "9em" }}>
               Next
