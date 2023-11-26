445b83ab3479188de4b8f8e88d84833b0b4dd0ee
Anuntayaice <iceanuntaya.5@gmail.com>
2023-11-26 21:35:19 +0100

    login card

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,6 +3,8 @@ import LoginCard from '../components/cards/LoginCard';
 
 export const Login = () => {
   return (
-    <div><LoginCard/></div>
-  )
+    <div className="bg-secondary d-flex align-items-center justify-content-center vh-100">
+      <LoginCard />
+    </div>
+  );
 }
