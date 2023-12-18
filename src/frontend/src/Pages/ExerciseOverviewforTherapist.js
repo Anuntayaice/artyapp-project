86d2d34520ab6e1cc40882fd4ff950f2beb5b55e
Anuntayaice <iceanuntaya.5@gmail.com>
2023-12-18 09:04:16 +0100

    bg edited

--- src/frontend/src/Pages/ExerciseOverviewforTherapist.js
+++ src/frontend/src/Pages/ExerciseOverviewforTherapist.js
@@ -52,7 +52,7 @@ const ExerciseOverviewforTherapist = () => {
 
   return (
     <div
-      className="bg-dark py-4 px-4 text-white"
+      className="bg-dark pt-4  px-4 text-white"
     >
       <h2>Select Exercise Type for {patient.patientName}</h2>
       <div className="container d-flex justify-content-center align-items-center ">
